dc97afe2753adc08ff6e6d78c3783e1558c9bc81
lebarba <lebarba@gmail.com>
2014-10-26 21:59:47 -0300

    Adding 3D texture converter

---
+++ 3DTextureConverter/3DtextureConverter/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace _3DtextureConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.depthTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.flipYcheckBox = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Enabled = false;
+            this.widthTextBox.Location = new System.Drawing.Point(53, 51);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(42, 20);
+            this.widthTextBox.TabIndex = 3;
+            this.widthTextBox.Text = "256";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Width:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Heigth:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Enabled = false;
+            this.heightTextBox.Location = new System.Drawing.Point(145, 51);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(42, 20);
+            this.heightTextBox.TabIndex = 5;
+            this.heightTextBox.Text = "256";
+            this.heightTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // depthTextBox
+            // 
+            this.depthTextBox.Location = new System.Drawing.Point(238, 51);
+            this.depthTextBox.Name = "depthTextBox";
+            this.depthTextBox.Size = new System.Drawing.Size(42, 20);
+            this.depthTextBox.TabIndex = 5;
+            this.depthTextBox.Text = "256";
+            this.depthTextBox.Leave += new System.EventHandler(this.depthTextBox_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Depth:";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 114);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(440, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // flipYcheckBox
+            // 
+            this.flipYcheckBox.AutoSize = true;
+            this.flipYcheckBox.Checked = true;
+            this.flipYcheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.flipYcheckBox.Location = new System.Drawing.Point(15, 77);
+            this.flipYcheckBox.Name = "flipYcheckBox";
+            this.flipYcheckBox.Size = new System.Drawing.Size(52, 17);
+            this.flipYcheckBox.TabIndex = 8;
+            this.flipYcheckBox.Text = "Flip Y";
+            this.flipYcheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "File:";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 190);
+            this.Controls.Add(this.flipYcheckBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.depthTextBox);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.widthTextBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Lebarba - Voxel Raw to Png mosaic converter ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.TextBox depthTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox flipYcheckBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 3DTextureConverter/3DtextureConverter/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace _3DtextureConverter
+{
+    /// <summary>
+    ///  Raw to PNG converter by Leandro Barbagallo (lebarba)
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String path = textBox1.Text;
+
+
+            int width = int.Parse( widthTextBox.Text);
+            int height = int.Parse(heightTextBox.Text); ;
+            int depth = int.Parse(depthTextBox.Text); ;
+
+            //To convert raw files larger than 256x256x256 you can tweak these values.
+            const int maxTilesWidth = 16;
+            const int maxTilesHeight = 16;
+
+            int newWidth = width * maxTilesWidth;
+            int newHeight = height * maxTilesHeight;
+
+
+            progressBar1.Maximum = width * height * depth;
+
+            using (Bitmap b = new Bitmap(newWidth, newHeight))
+            {
+                using (Graphics g = Graphics.FromImage(b))
+                {
+
+                    try
+                    {
+                        
+                        using (BinaryReader breader = new BinaryReader(File.Open(path, FileMode.Open)))
+                        {
+
+                            bool flipY = flipYcheckBox.Checked;
+
+                            int length = (int)breader.BaseStream.Length;
+
+                            int realPosX, realPosY;
+
+                            for (int z = 0; z < depth; z++)
+                            {
+                                for (int y = 0; y < height; y++)
+                                {
+                                    for (int x = 0; x < width; x++)
+                                    {
+                                        int value = breader.ReadByte();
+
+                                        realPosX = x + (z % maxTilesHeight) * width;
+
+                                        int yPos;
+                                        if (flipY)
+                                        {
+                                            yPos = height - y - 1;
+                                        }
+                                        else
+                                        {
+                                            yPos = y;
+                                        }
+
+                                        realPosY = yPos + ((int)(z / maxTilesWidth)) * height;
+                                        b.SetPixel(realPosX, realPosY, Color.FromArgb(value, value, value, value));
+
+                                    }
+
+                                }
+                                progressBar1.Value = z * width * height;
+                                progressBar1.Update();
+                            }
+
+                        }
+
+                        //TODO: Encode only A8.
+                        b.Save(path + ".png", ImageFormat.Png);
+
+                        progressBar1.Value = depth * width * height;
+                        progressBar1.Update();
+
+                        MessageBox.Show("Done converting " + path + ".png");
+
+                    
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("error reading content. Check that file exists and that the dimensions are correct.");
+                    }
+
+                    progressBar1.Value = 0;
+                    progressBar1.Update();
+                }
+
+                
+            }
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = textBox1.Text ;
+            openFileDialog1.Filter = "raw files (*.raw)|*.raw|All files (*.*)|*.*" ;
+            openFileDialog1.FilterIndex = 1 ;
+            openFileDialog1.RestoreDirectory = true ;
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                button1.Enabled = true;
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void depthTextBox_Leave(object sender, EventArgs e)
+        {
+            int val;
+
+            try
+            {
+                val = int.Parse(depthTextBox.Text);
+                if (val <= 0)
+                    throw new Exception();
+
+                if (val > 256)
+                    throw new Exception();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Invalid depth.");
+                depthTextBox.Text = "256";
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+
+    }
+}
